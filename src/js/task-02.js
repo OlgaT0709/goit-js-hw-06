bca61a4fd75de6d1560fde5afdc5be0ccbcd966d
Olga Tolstykhina <ovt0709@gmail.com>
2023-02-15 22:37:09 +0200

    task02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulRef = document.querySelector('#ingredients');
+
+const liesRef = ingredients.map( ingredient => {
+  const liRef = document.createElement('li');
+  liRef.textContent = ingredient;
+  liRef.classList.add('item');
+  return liRef;
+});
+
+ulRef.append(...liesRef );
+console.log(ulRef);
