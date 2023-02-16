22b02d7810385f25f6e05e858d85a2607dca2c34
Olga Tolstykhina <ovt0709@gmail.com>
2023-02-16 23:23:05 +0200

    task05

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,1 +1,11 @@
+const inputName = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+
+inputName.addEventListener("input", event => {
+    event.currentTarget.value === ""
+    ? outputName.textContent = "Anonymous"
+    : outputName.textContent = event.currentTarget.value;
+    
+});
+
 
