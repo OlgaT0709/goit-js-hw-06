f54cd9a430d86905d4efd106eebc19146b04826d
Olga Tolstykhina <ovt0709@gmail.com>
2023-02-16 17:24:54 +0200

    task03 another method

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -13,22 +13,28 @@ const images = [
   },
 ];
 
+// const ulRef = document.querySelector('.gallery');
+// const liesRef = images.map(({ url, alt }) => {
+//   const liRef = document.createElement('li');
+//   const imgRef = document.createElement('img');
+//   imgRef.src = url;
+//   imgRef.alt = alt;
+//   imgRef.style.display = 'block';
+//   imgRef.style.width = '100%';
+//   imgRef.style.marginBottom = '10px'
+
+//   liRef.append(imgRef);
+//   return liRef;
+// });
+
+// ulRef.append(...liesRef );
+// console.log(ulRef);
+
 const ulRef = document.querySelector('.gallery');
-const liesRef = images.map(({ url, alt }) => {
-  const liRef = document.createElement('li');
-  const imgRef = document.createElement('img');
-  imgRef.src = url;
-  imgRef.alt = alt;
-  imgRef.style.display = 'block';
-  imgRef.style.width = '100%';
-  imgRef.style.marginBottom = '10px'
 
-  
-  liRef.append(imgRef);
-  return liRef;
-});
+const liRef = images.map(({ url, alt }) => 
+  `<li><img src=${url} alt=${alt} style="display: block; width: 100%; margin-bottom: 10px;"></li>`
+);
 
-ulRef.append(...liesRef );
+ulRef.insertAdjacentHTML('afterbegin', liRef.join(''));
 console.log(ulRef);
-
-// ulRef.insertAdjacentHTML('afterbegin', '<li> .... </li>');
