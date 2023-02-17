95901a18867d01a2bb7313d261d2233b1caeef12
Olga Tolstykhina <ovt0709@gmail.com>
2023-02-17 22:53:54 +0200

    task07 fixed

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -2,7 +2,7 @@ const inputRef = document.querySelector('#font-size-control');
 const textRef = document.querySelector('#text');
 textRef.style.fontSize = `${inputRef.value}px`;
 
-inputRef.addEventListener('change', textSizeChange);
+inputRef.addEventListener('input', textSizeChange);
 
 function textSizeChange(event) {
     textRef.style.fontSize = `${event.currentTarget.value}px`;
