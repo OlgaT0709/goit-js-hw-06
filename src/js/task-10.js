bad9c796cc941abf7dc2605e89632e1fc22d91c2
Olga Tolstykhina <ovt0709@gmail.com>
2023-02-17 15:01:43 +0200

    task10 simplified

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,59 +3,41 @@ const refs = {
   btnCreate: document.querySelector('[data-create]'),
   btnDestroy: document.querySelector('[data-destroy]'),
   box: document.querySelector('#boxes'),
-  
 };
 
-refs.box.style.position = "relative"
+refs.box.style.position = 'relative';
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
 function createBoxes(amount) {
+  refs.box.innerHTML = '';
   for (let i = 0; i < amount; i += 1) {
-  const size = 30 + i * 10;
-  const divEl = document.createElement('div');
-  divEl.style.width = `${size}px`;
-  divEl.style.height = `${size}px`;
-  divEl.style.backgroundColor = getRandomHexColor();
-  divEl.style.position = 'absolute';
-  divEl.style.top = `${size}px`;
-  divEl.style.left = `${size}px`;
-  refs.box.appendChild(divEl);
+    const size = 30 + i * 10;
+    const divEl = document.createElement('div');
+    divEl.style.width = `${size}px`;
+    divEl.style.height = `${size}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    divEl.style.position = 'absolute';
+    divEl.style.top = `${size}px`;
+    divEl.style.left = `${size}px`;
+    refs.box.appendChild(divEl);
   }
+}
+
+function onCreateBoxes() {
+  console.log(refs.input.value);
+  createBoxes(refs.input.value);
 };
 
-function destroyBoxes(amount) {
-  if (amount === 0) {
-    return;
-  }
-  for (let i = 1; i <= amount; i += 1) {
-    const divEl = document.querySelector('#boxes div');
-    divEl.remove();
-    
-  }
+function onDestroyBoxes() {
+  refs.input.value = '';
+  refs.box.innerHTML = '';  
 };
-
-refs.input.addEventListener('blur', (event) => {
-  let amount = 0;
-  amount = Number(event.currentTarget.value);
 
 refs.btnCreate.addEventListener('click', onCreateBoxes);
 refs.btnDestroy.addEventListener('click', onDestroyBoxes);
-  
-  function onCreateBoxes() {
-    console.log(amount);
-    createBoxes(amount);
- };
-
-function onDestroyBoxes() {
-destroyBoxes(amount)  
-};
-});
-
-
-
 
 
 
