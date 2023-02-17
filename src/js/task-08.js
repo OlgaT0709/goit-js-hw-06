206264a4036c4081df46d9842d65198a47a6e79d
Olga Tolstykhina <ovt0709@gmail.com>
2023-02-17 02:51:19 +0200

    task08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,33 @@
+const formRef = document.querySelector('.login-form');
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+   
+    if (formElements.email.value === '' || formElements.password.value === '') {
+        alert('Всі поля повинні бути заповнені.');
+       
+    } else {
+
+        const formData = {};
+        formData.email = formElements.email.value;
+        formData.password = formElements.password.value;
+        console.log(formData);
+
+        // const formData = new FormData(event.currentTarget);
+    
+        // formData.forEach((value, name) => {
+        //     const result = {};
+        //     result[name] = value;
+        //     console.log(result);
+        // });
+        
+        event.currentTarget.reset();
+    };
+ 
+    
+};
+
 
