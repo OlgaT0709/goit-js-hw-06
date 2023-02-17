d6751e712625f5dd95ec4a82adfc236b38cdc227
Olga Tolstykhina <ovt0709@gmail.com>
2023-02-17 03:11:42 +0200

    task09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColorRef = document.querySelector('.change-color');
+const colorDescriptionRef = document.querySelector('.color')
+const bodyRef =  document.querySelector('body')
+
+btnChangeColorRef.addEventListener('click', changeColor);
+
+function changeColor(event ) {
+  
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  colorDescriptionRef.textContent = bodyRef.style.backgroundColor;
+};
+
+
