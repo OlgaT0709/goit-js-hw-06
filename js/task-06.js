6e8c1b2673c1b840ae9026a99f75632be7a21a60
Olga Tolstykhina <ovt0709@gmail.com>
2023-02-19 17:55:25 +0200

    task06 update

--- js/task-06.js
+++ js/task-06.js
@@ -2,18 +2,22 @@ const inputRef = document.querySelector('#validation-input')
 inputRef.addEventListener('blur', onInputBlur);
 
 function onInputBlur(event) {
-    event.currentTarget.value.length === Number(inputRef.dataset.length)
-        ? inputRef.classList.add('valid')
-        : inputRef.classList.add('invalid');   
+    if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
+        inputRef.classList.add('valid');
+        inputRef.classList.remove('invalid');
+    } else {
+        inputRef.classList.add('invalid');
+        inputRef.classList.remove('valid');
+    }
      
 };
 
-inputRef.addEventListener('focus', onInputFocus);
+// inputRef.addEventListener('focus', onInputFocus);
 
-function onInputFocus(event) {
-    if (inputRef.classList.value === 'invalid') {
-        inputRef.classList.remove('invalid');
-    } else if (inputRef.classList.value === 'valid'){
-         inputRef.classList.remove('valid');
-    }
-};
+// function onInputFocus(event) {
+//     if (inputRef.classList.value === 'invalid') {
+//         inputRef.classList.remove('invalid');
+//     } else if (inputRef.classList.value === 'valid'){
+//          inputRef.classList.remove('valid');
+//     }
+// };
